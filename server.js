fa62ccc2bd3552baa52ed957b622548db8291e5a
Garves <garves44@gmail.com>
2020-07-14 11:39:34 -0600

    added routes for animal ID

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-// Filter function
+// Find by queryParam
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
 
@@ -42,6 +42,12 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+//Find by ID
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 // Route request for animals.json
 app.get('/api/animals/', (req, res) => {
     let results = animals;
@@ -50,6 +56,16 @@ app.get('/api/animals/', (req, res) => {
     }
     res.json(results);
 });
+
+//Route request for specific animal or ID
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+})
 
 
 app.listen(PORT, () => {
