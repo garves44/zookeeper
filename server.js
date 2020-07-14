b9e1ff618a7e4607f47335b1b2134c9c13ac4e59
Garves <garves44@gmail.com>
2020-07-14 15:05:19 -0600

    added zookeeper page and routes

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const {
 const fs = require('fs');
 const path = require('path');
 
+//Routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,123 +18,11 @@ app.use(express.urlencoded({
 }));
 //Parse incoming JSON data
 app.use(express.json());
+app.use('./api', apiRoutes);
+app.use('/', htmlRoutes);
 //ALlow server to send css and js files without creating routes for each one
 app.use(express.static('public'));
 
-
-// Find by queryParam
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        //If personalityTraits is a string, place into new array and save.
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-    }
-    //Loop through each trait in personalityTraitsArray
-    personalityTraitsArray.forEach(trait => {
-        filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-        );
-    });
-
-    //Filtering by diet
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    //Filtering by species
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    //Filtering by name
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-};
-
-//Find by ID
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-};
-
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(path.join(__dirname, './data/animals.json'),
-        JSON.stringify({
-            animals: animalsArray
-        }, null, 2)
-    );
-    return animal;
-};
-
-//Validating information
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-        return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-        return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-        return false;
-    }
-    if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
-        return false;
-    }
-
-    return true;
-};
-
-// Route request for animals.json
-app.get('/api/animals/', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-//Route request for specific animal or ID
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-//Post route to send data
-app.post('/api/animals', (req, res) => {
-    //set id based on next index value
-    req.body.id = animals.length.toString();
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The aniomal is not properly formatted.');
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-
-        res.json(animal);
-    }
-});
-//Get route to serve index.html as home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-//Get route to serve animals.html as /animals page
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-//Get route to serve zookeepers.html as /zookeepers page
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
